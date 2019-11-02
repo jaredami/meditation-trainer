2da6163139c0aa762f114721942b21cac79121bc
Jared Storm <jaredstorm@jareds-mbp.attlocal.net>
2019-11-02 14:25:05 -0400

    Add timers

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainer.component.scss']
 })
 export class TrainerComponent implements OnInit {
+  currentStreakTimer = '0';
+  sessionTimer = '5:00';
+  seconds = 0;
+  minutes = 0;
+  currentMax = 10;
+  startStreakTimer;
+  sessionStarted = false;
+  seconds2 = 0;
+  minutes2 = 20;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.startStreakTimer = setInterval(() => { this.addTime(); }, 1000);
   }
 
+  addTime() {
+    console.log('addTime called');
+    console.log('seconds', this.seconds);
+    if (this.seconds < this.currentMax) {
+      this.seconds++;
+      if (this.seconds >= 60) {
+        this.seconds = 0;
+        this.minutes++;
+      }
+      this.currentStreakTimer =
+        (this.minutes ? (this.minutes > 9 ? this.minutes : '0' + this.minutes) : '00') +
+        ':' +
+        (this.seconds > 9 ? this.seconds : '0' + this.seconds);
+    } else {
+      // BUG: doesn't hit this if currentMax is greater than 60
+      clearInterval(this.startStreakTimer);
+    }
+  }
 }
