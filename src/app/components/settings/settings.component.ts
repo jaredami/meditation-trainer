72710693a2a9aa91b6a06260fd7b690fed561089
Jared Storm <jaredrstorm@gmail.com>
2020-01-03 13:55:31 -0500

    Add ability to preview sound selections in settings

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 import { Settings } from 'src/app/models/settings.model';
 
@@ -9,7 +9,12 @@ import { Settings } from 'src/app/models/settings.model';
 })
 export class SettingsComponent implements OnInit {
   settings: Settings;
+
   periodEndSoundSrc: string;
+  sessionEndSoundSrc: string;
+
+  @ViewChild('periodAudio') periodAudioRef: ElementRef;
+  @ViewChild('sessionAudio') sessionAudioRef: ElementRef;
 
   constructor(
     private settingsService: SettingsService
@@ -19,7 +24,7 @@ export class SettingsComponent implements OnInit {
     this.settingsService.settingsChanges.subscribe(update => this.settings = update);
   }
 
-  updateSettings(setting: string, increase: boolean) {
+  incrementSetting(setting: string, increase: boolean) {
     if (increase) {
       this.settingsService.setSettings({
         [setting]: this.settings[setting] + 1
@@ -32,14 +37,28 @@ export class SettingsComponent implements OnInit {
   }
 
   updatePeriodEndSound(newSrc) {
+    this.periodEndSoundSrc = newSrc;
+    this.playPeriodEndSound();
     this.settingsService.setSettings({
-      periodEndSoundSrc: newSrc
+      periodEndSoundSrc: this.periodEndSoundSrc
     });
   }
 
   updateSessionEndSound(newSrc) {
+    this.sessionEndSoundSrc = newSrc;
+    this.playSessionEndSound();
     this.settingsService.setSettings({
       sessionEndSoundSrc: newSrc
     });
+  }
+
+  private playPeriodEndSound() {
+    this.periodAudioRef.nativeElement.load();
+    this.periodAudioRef.nativeElement.play();
+  }
+
+  private playSessionEndSound() {
+    this.sessionAudioRef.nativeElement.load();
+    this.sessionAudioRef.nativeElement.play();
   }
 }
