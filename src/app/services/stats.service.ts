f06db2472167ffe8132ae363544f46de9c73e5ef
Jared Storm <jaredrstorm@gmail.com>
2020-01-18 22:43:43 -0500

    Refactor stats service

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -10,51 +10,50 @@ export class StatsService {
   // TODO: store date of the first login for this
   private dateStarted: number = 1576818000000;
   private today: number = Date.now();
+  private increment: firestore.FieldValue = firestore.FieldValue.increment(1);
 
   constructor(
-    private firestore: AngularFirestore
-  ) {}
+    private afs: AngularFirestore
+  ) {
+  }
 
   getStats(): Observable<Action<DocumentSnapshot<{}>>> {
-    return this.firestore.doc('users/user1').snapshotChanges();
+    return this.afs.doc('users/user1').snapshotChanges();
   }
 
   addTotalSessionTime(): void {
-    const increment: firestore.FieldValue = firestore.FieldValue.increment(1);
-    this.firestore.doc('users/user1').update({ 'stats.totalSessionTime': increment });
+    this.afs.doc('users/user1').update({ 'stats.totalSessionTime': this.increment });
   }
 
   addCompletedSession(): void {
-    const increment: firestore.FieldValue = firestore.FieldValue.increment(1);
-    this.firestore.doc('users/user1').update({ 'stats.completedSessions': increment });
+    this.afs.doc('users/user1').update({ 'stats.completedSessions': this.increment });
   }
 
   checkForLongestPeriod(currentMaxPeriodLength: number): void {
-    this.firestore.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
+    this.afs.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
       const data: firestore.DocumentData = doc.data();
       if (data.stats.longestPeriod < currentMaxPeriodLength) {
-        this.firestore.doc('users/user1').update({ 'stats.longestPeriod': currentMaxPeriodLength });
+        this.afs.doc('users/user1').update({ 'stats.longestPeriod': currentMaxPeriodLength });
       }
     });
   }
 
   checkForLongestSessionCompleted(currentSessionLength: number): void {
-    this.firestore.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
+    this.afs.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
       const data: firestore.DocumentData = doc.data();
       if (data.stats.longestSession < currentSessionLength) {
-        this.firestore.doc('users/user1').update({ 'stats.longestSession': currentSessionLength });
+        this.afs.doc('users/user1').update({ 'stats.longestSession': currentSessionLength });
       }
     });
   }
 
   setAverageMinutesPerDay(): void {
     let totalSessionTime: number;
-    this.firestore.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
-      const data: firestore.DocumentData = doc.data();
-      totalSessionTime = data.stats.totalSessionTime;
+    this.afs.doc('users/user1').ref.get().then((doc: firestore.DocumentSnapshot) => {
+      totalSessionTime = doc.data().stats.totalSessionTime;
       const dateDiff: number = Math.round((this.today - this.dateStarted) / (1000 * 60 * 60 * 24));
       const average: number = Math.round(totalSessionTime / dateDiff);
-      this.firestore.doc('users/user1').update({ 'stats.averageSessionTime': average });
+      this.afs.doc('users/user1').update({ 'stats.averageSessionTime': average });
     });
   }
 }
